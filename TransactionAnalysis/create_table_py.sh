47fbd6aacc4f15ddafb6db44e8b2c6c050acbbd0
C35612 <werever12>
2018-03-09 09:52:09 +0100

    removed folder

--- TransactionAnalysis/create_table_py.sh
+++
@@ -1,2 +0,0 @@
-
-"C:\Users\C35612.LAUNCHER\WinPython-64bit-3.5.3.1Qt5\python-3.5.3.amd64\python.exe" C:\Users\C35612.LAUNCHER\Production_code\Transaction_analyis\createTable.py
--- package_demos/test_sysarg.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-print(len(sys.argv))
-
-location_LOC_BAG = sys.argv[1]
-
-print(location_LOC_BAG)
-print(sys.argv)
