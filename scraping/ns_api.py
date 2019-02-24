16afc0e9db53762839be9ca2548bb72db748ed5d
Harrevelt <s.d.harrevelt@tue.nl>
2019-02-24 18:15:50 +0100

    almost done with NS api

--- scraping/ns_api.py
+++ scraping/ns_api.py
@@ -1,5 +1,6 @@
 # dd MM yyyy format
 
+import urllib.parse
 from datetime import datetime
 import numpy as np
 import api_secrets
@@ -7,6 +8,8 @@ import requests
 import pandas as pd
 import os
 import json
+import pytz
+
 from bs4 import BeautifulSoup as bs
 
 
@@ -16,10 +19,14 @@ file_dir = r'C:\Users\20184098\Documents\NS Busines'
 file_path = os.path.join(file_dir, file_name)
 
 auth_obj = {'username': api_secrets.NS_username, 'x-api-key': api_secrets.NS_API_KEY}
+headers_browser = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, '
+                                 'like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+auth_obj.update(headers_browser)
 
 # Prijzen-url
 basis_price_url = "https://ns-api.nl/reisinfo/api/v2/price"
 basis_station_url = "https://ns-api.nl/reisinfo/api/v2/stations"
+basis_trips_url = "https://ns-api.nl/reisinfo/api/v3/trips"
 
 # Station lijst
 json_station = json.loads(requests.get(basis_station_url, headers=auth_obj).text)
@@ -38,15 +45,24 @@ for i, i_row in travel_list.iterrows():
         date = i_row['Datum']
         check_in = i_row['Check in']
         check_uit = i_row['Check uit']
-        check_in_iso = str(datetime.strptime(date + check_in, "%d-%m-%y%H:%M"))
-        check_uit_iso = str(datetime.strptime(date + check_uit, "%d-%m-%y%H:%M"))
+        check_in_iso = str(datetime.strptime(date + check_in, "%d-%m-%y%H:%M").astimezone()).replace(' ', 'T')
+        check_uit_iso = str(datetime.strptime(date + check_uit, "%d-%m-%y%H:%M").astimezone()).replace(' ', 'T')
     else:
         continue
 
-    travel_url = "?fromStation={stn1}&toStation={stn2}&plannedFromTime={checkin}".format(stn1=station_code[stn1],
-                                                                                    stn2=station_code[stn2],
-                                                                                    checkin=check_in_iso[
-                                                                                            :-3].replace(' ', 'T'))
-    price_url = basis_price_url + travel_url
+    get_data = {'fromStation': stn1, 'toStation': stn2, 'plannedFromTime': check_in_iso}
+    res = requests.get(basis_trips_url, params=get_data, headers=auth_obj)
+    res.json()
 
-    res = requests.get(price_url, headers=auth_obj)
+    for i in dict(res.json())['trips'][0].keys():
+        print(i)
+
+    # Oke ben er bijna... nu alleen nog ff de juiste class erbij pakken... en dna klaar
+
+    sel_travelClass = 'SECOND_CLASS'
+    sel_discountType = 'NO_DISCOUNT'
+    sel_discountType = 'DISCOUNT_40_PERCENT'
+    sel_product = 'OVCHIPKAART_ENKELE_REIS'
+
+for i in dict(res.json())['trips'][0]['fares']:
+    print(i)
